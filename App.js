3958b4846b136891925187f5f3c4fc7c1c6c0a19
Brent Vatne <brentvatne@gmail.com>
2020-12-01 10:49:02 -0800

    Add example

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
+import * as ErrorRecovery from 'expo-error-recovery';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  async componentDidMount() {
+    console.log(this.props.exp.errorRecovery);
+  }
+
+  async setRecoveryProps() {
+    await ErrorRecovery.setRecoveryProps({
+      props: 'recoveryProps',
+    });
+  }
+
+  getRecoveryProps() {
+    console.log(ErrorRecovery.recoveredProps);
+  }
+
+  throwError() {
+    throw new Error('Error has been thrown.');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button onPress={this.setRecoveryProps} title="setRecoveryProps" />
+        <Button onPress={this.getRecoveryProps} title="getProps" />
+        <Button onPress={this.throwError} title="throwError" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  }
 });
